ca9f1e0813fd0eea6d497bc94b1c524d712f6ce8
rino86wn <rino.86wn@gmail.com>
2022-04-20 20:57:55 +0900

    1-6 styled-componentsの導入をしてスタイリングする

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 //ライブラリをインポートする宣言
 import * as React from 'react'
 import { render } from 'react-dom'
+import styled from "styled-components";
 
-//Mainという変数にReactのビューを書いている
-const Main = (<h1>Markdown Editor</h1>)
+// //Mainという変数にReactのビューを書いている
+// const Main = (<h1>Markdown Editor + dev serve</h1>)
+
+//スタイルングされたコンポーネントを作成
+//styled.（htmlのタグ名）で生成したいHTMLタグを指定して、その後続く``内にCSSを記述すると
+//そのコンポーネントにスタイルが適応される仕組み
+const Header = styled.h1`
+   color:red;
+   `
+const Main = (<Header>Markdown Editor</Header>)
 
 //ReactとHTMLファイルを繋ぐ処理
 //appというIDを持つHTML要素に対してMainという変数の内容を紐づける
